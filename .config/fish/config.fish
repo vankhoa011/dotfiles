fefa44b59eaceb324e78eed95d4f4131a10889bf
khoa <vankhoa011@gmail.com>
2023-12-06 19:50:27 -0800

    Add config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,4 +12,5 @@ set fish_cursor_insert line
 set fish_cursor_replace_one underscore
 set fish_cursor_visual block
 set fish_vi_force_cursor true
+test -f ~/.config/fish/.kubectl_aliases.fish && source ~/.config/fish/.kubectl_aliases.fish
 starship init fish | source
